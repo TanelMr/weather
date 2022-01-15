034ea1e878e0e683ff1e1843091fa1693f957e74
Tanel Märjama <tanel.marjama@khk.ee>
2022-01-15 19:30:57 +0200

    Displayed city weather data from API

--- app.js
+++ app.js
@@ -9,10 +9,19 @@ function weatherDataFetch(city){
         })
         .then(function (data){
             console.log(data);
+            drawWeather(data);
         })
         .catch(function() {
         })
 }
 function cityWeather(event) {
-    weatherDataFetch("Tallinn");
+    weatherDataFetch("Tartu");
+}
+
+function drawWeather (data){
+    let celsius = Math.round(parseFloat(data.main.temp)-273.15);
+    let description = data.weather[0].description;
+    document.querySelector("#description").innerHTML = description;
+    document.querySelector("#temp").innerHTML = celsius + "&deg;";
+    document.querySelector("#location").innerHTML = data.name;
 }
