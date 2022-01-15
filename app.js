c4de668b45fa1c9c2de6395cce63f819a6062a06
Tanel Märjama <tanel.marjama@khk.ee>
2022-01-15 20:45:31 +0200

    Repaired weatherDataFetch functionality

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ document.addEventListener("DOMContentLoaded", cityWeather)
 
 function weatherDataFetch(city){
     const key = "8b0d1e31e286ec235fba8e3a8345ece8";
-    // for some reason, using variables didn't work, added key and city directly to fetch request
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=Tartu&appid=8b0d1e31e286ec235fba8e3a8345ece8")
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
         .then(function(resp) {
             //convert data to json
         return resp.json();
